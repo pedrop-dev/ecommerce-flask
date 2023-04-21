ed371e12b503330ae44548c5efe4f0d3d432bcc6
Heitor Prado <ubuntuwsl@LAPTOP-EESUVLQV.localdomain>
2023-04-21 14:18:00 -0300

    Add login backend

--- db.py
+++ db.py
@@ -28,7 +28,7 @@ def init_db():
 @click.command('init-db')
 def init_db_command():
     """ Clear existing data and create new tables ( read schema.sql ) """
-    init_db();
+    init_db()
     click.echo('Initialized the database')
 
 def init_app(app):
