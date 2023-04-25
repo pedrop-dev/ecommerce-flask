92467cac096f84907d6374798c5a513000cc2a77
Pedro Henrique <92335472+pedrop-dev@users.noreply.github.com>
2023-04-25 19:39:07 -0300

    Cart first style

--- static/script/home.js
+++ static/script/home.js
@@ -2,3 +2,18 @@ function showMenu() {
     navBar = document.querySelector(".nav-bar");
     navBar.classList.toggle("active");
 }
+
+//Cart
+
+function openCart() {
+    const cartDisapear = document.querySelector('#cart-disapear')
+    //const cartItems = document.querySelector('.cart-items')
+
+    cartDisapear.style.display = 'block'
+    //cartItems.style.display = 'block'
+}
+
+function closeCart() {  
+    const cartDisapear = document.querySelector('#cart-disapear')
+    cartDisapear.style.display = 'none'
+}
